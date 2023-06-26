ee4c7777faeccc3ccc4169ece8bc0435cf4f801c
Mahroof vp <mahroofvp77@gmail.com>
2023-06-26 17:28:09 +0530

    added empty cart img

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -3,14 +3,18 @@ import { PRODUCTS } from '../../products'
 import { ShopContext } from '../../context/ShopContext'
 import { CartItem } from '../../components/cartItem/CartItem'
 import './cart.css'
+import emptyCart from '../../assets/bgimg/EmptyCart.png'
+import {useNavigate} from 'react-router-dom'
 
 
 const Cart = () => {
   const {cartItems, getTotalCartAmount} = useContext(ShopContext)
+  const totalAmount = getTotalCartAmount()
+  const navigate = useNavigate()
   return (
     <div className='cart'>
       <div>
-        <h1> Your Cart Items </h1>
+        {/* <h1> Your Cart Items </h1>  */}
       </div>
       <div className="cartItems">
         {PRODUCTS.map((data)=>{
@@ -20,14 +24,13 @@ const Cart = () => {
         })}
       </div>
 
-      <div className="checkout">
-        <p> Subtotal: {getTotalCartAmount()}</p>
+      {totalAmount>0 ?<div className="checkout">
+        <p> Subtotal: ${totalAmount}</p>
         <div className='cart-black-btns'>
 
-        <button > Continue Shopping</button>
-        <button> Cheakout </button>
+        <button onClick={()=> navigate('/')} > Continue Shopping</button>
         </div>
-      </div>
+      </div>: <div className='empty-cart-img'><img src={emptyCart} alt="" /></div> }
 
     </div>
   )
