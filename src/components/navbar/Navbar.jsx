aee04a90dd0ff2e43e7979c9933edb79ebf227af
Mahroof VP <mahroofvp77@gmail.com>
2024-04-06 18:27:37 +0530

    logo has changed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { BsCart3 } from 'react-icons/bs'
 import { useContext } from 'react'
 import { ShopContext } from '../../context/ShopContext'
 import { PRODUCTS } from '../../products'
+import logo from '../../../public/Logo.png'
 import './navbar.scss'
 
 
@@ -15,13 +16,16 @@ let total = Object.values(cartItems).reduce((acc,val)=> acc+val,0)
 
 return (
     <div className="navbar">
-      <div><Link to="/" style={{textDecoration:"none", opacity:"80%"}}><h1 >Gadget Gallery</h1></Link></div>
+      <div><Link to="/" style={{textDecoration:"none", opacity:"80%" ,marginLeft:"15px"}}> <img style={{height:"40px", width:"80px"}} src={logo} alt="" /> </Link></div>
         <div className="links">
            
           { !isHomeRoute && <Link to="/" > Shop </Link>}
             <Link to="/cart" >
               
-           {total>0?<div className='badge'> {total}</div>:null }
+           {
+            
+           total>0?<div className='badge'> {total}</div>:null 
+           }
             <BsCart3 style={{height:"25px", width:"25px"}} className='cart-icon' size={32}/>
              
              </Link>
